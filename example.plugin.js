842222a98879fbeafafd82143deba0d0b110ecf5
tony311 <tony@tonyfox.ws>
2017-01-11 10:04:10 -0500

    Updated to use new bdPluginStorage in BD 1.76

--- example.plugin.js
+++ example.plugin.js
@@ -123,7 +123,7 @@ optionsExample.prototype.getOptionsPlugin  = function(){
 	OptionsPlugin.prototype.getAll         = function(){             // Get all options
 		return this.options;
 	};
-	OptionsPlugin.prototype.simpleOptions  = function(){             // Get options as a key=value object (used for localStorage)
+	OptionsPlugin.prototype.simpleOptions  = function(){             // Get options as a key=value object (used for bdPluginStorage)
 		var simpleoptions = {};
 		
 		$.each(this.options, function(key, option){
@@ -132,13 +132,13 @@ optionsExample.prototype.getOptionsPlugin  = function(){
 		
 		return simpleoptions;
 	};
-	OptionsPlugin.prototype.save           = function(){             // Save options to localStorage
-		localStorage.setItem(this.storageKey, JSON.stringify(this.simpleOptions()));
+	OptionsPlugin.prototype.save           = function(){             // Save options to bdPluginStorage
+		bdPluginStorage.set(this.storageKey, JSON.stringify(this.simpleOptions()));
 	};
-	OptionsPlugin.prototype.load           = function(){             // Load options from localStorage
+	OptionsPlugin.prototype.load           = function(){             // Load options from bdPluginStorage
 		this.options = this._clone(this.defaults);
 		
-		var loaded = JSON.parse(localStorage.getItem(this.storageKey));
+		var loaded = JSON.parse(bdPluginStorage.get(this.storageKey));
 		if (!loaded) return;
 		
 		var self = this;
